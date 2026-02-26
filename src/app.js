6aa450ceb60392dd4853be80cba977c6f75fc89e
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2026-02-27 00:52:39 +0100

    Add user/auth routes and Swagger setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const setupSwagger = require("./config/swagger");
 
 const app = express();
 
+const userRoutes = require("./routes/user.routes");
+const authRoutes = require("./routes/auth.routes");
+
 app.use(express.json());
+
+// Routes
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+// Swagger
+setupSwagger(app);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "HIIII" });
