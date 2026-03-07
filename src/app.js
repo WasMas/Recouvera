5028b5953235e338c83474315bec9a529b5568aa
WasMas <106668591+WasMas@users.noreply.github.com>
2026-03-07 21:27:30 +0100

    Register client routes

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ const app = express();
 
 const userRoutes = require("./routes/user.routes");
 const authRoutes = require("./routes/auth.routes");
+const clientRoutes = require("./routes/client.routes");
 
 app.use(express.json());
 
 // Routes
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/clients", clientRoutes);
 
 // Swagger
 setupSwagger(app);
